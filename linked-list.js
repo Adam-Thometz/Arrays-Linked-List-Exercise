f4a8f5f1e359bb73ef413d86919246fc9d3039b6
Adam Thometz <adamthometz@gmail.com>
2021-09-16 17:58:58 -0400

    finished assignment

--- linked-list.js
+++ linked-list.js
@@ -18,6 +18,22 @@ class LinkedList {
     for (let val of vals) this.push(val);
   }
 
+  /** HELPER: getNode(idx): Get a node
+   * This is different from getAt. getAt returns a value while getNode returns the node
+   */
+
+  getNode(idx) {
+    let node = this.head
+    let count = 0
+
+    while (node !== null && count !== idx) {
+      count++
+      node = node.next
+    }
+
+    return node
+  }
+
   /** push(val): add new value to end of list. */
 
   push(val) {
@@ -49,50 +65,93 @@ class LinkedList {
   /** pop(): return & remove last item. */
 
   pop() {
-    if (this.length === 0) throw new Error("Cannot pop from an empty list")
-    let node = this.head
-    while (node.next) {
-      node = node.next
-    }
-    node.next
-    this.tail = node
-    return node
+    return this.removeAt(this.length - 1)
   }
 
   /** shift(): return & remove first item. */
 
   shift() {
-
+    return this.removeAt(0)
   }
 
   /** getAt(idx): get val at idx. */
 
   getAt(idx) {
+    if (idx < 0 || idx > this.length - 1) throw new Error("Invalid index")
 
+    return this.getNode(idx).val
   }
 
   /** setAt(idx, val): set val at idx to val */
 
   setAt(idx, val) {
+    if (idx < 0 || idx > this.length - 1) throw new Error("Invalid index")
 
+    const node = this.getNode(idx)
+    node.val = val
   }
 
   /** insertAt(idx, val): add node w/val before idx. */
 
   insertAt(idx, val) {
+    if (idx < 0 || idx > this.length) throw new Error("Invalid index")
 
+    if (idx === 0) return this.unshift(val)
+    if (idx === this.length) return this.push(val)
+
+    const prev = this.getNode(idx - 1)
+
+    const newNode = new Node(val)
+    newNode.next = prev.next
+    prev.next = newNode
+
+    this.length++
   }
 
   /** removeAt(idx): return & remove item at idx, */
 
   removeAt(idx) {
+    if (idx < 0 || idx >= this.length) throw new Error('Invalid index')
 
+    // delete first element
+    if (idx === 0) {
+      const val = this.head.val
+      this.head = this.head.next
+      this.length--
+      if (this.length < 2) this.tail = this.head
+      return val
+    }
+
+    const prev = this.getNode(idx - 1)
+    // delete last element
+    if (idx === this.length - 1) {
+      const val = prev.next.val
+      prev.next = null
+      this.tail = prev
+      this.length--
+      return val
+    }
+
+    // all other cases
+    const val = prev.next.val
+    prev.next = prev.next.next
+    this.length--
+    return val
   }
 
   /** average(): return an average of all values in the list */
 
   average() {
-    
+    if (this.length === 0) return 0
+    let sum = 0;
+    let curr = this.head
+
+    while (curr) {
+      sum += curr.val
+      curr = curr.next
+    }
+
+    return sum / this.length
   }
 }
 
